d2162c24695e10c6ed4bcca7332600a79030c9d8
hamzaelboukri <hamzaelboukri01@gmail.com>
2024-11-11 17:38:28 +0100

    addr

--- js.js
+++ js.js
@@ -1,4 +1,26 @@
 
+window.onload = function() {
+  const today = new Date().toISOString().split('T')[0];
+  document.getElementById('departuredate').setAttribute('min', today);
+
+  
+  const inputs = document.querySelectorAll(".inputs");
+  inputs.forEach(input => {
+    input.addEventListener('change', function () {
+    
+      const label = input.closest("label");
+      const startdate = label.querySelector('.start-date' + input.id.replace('select', ''))?.innerText;
+      const enddate = label.querySelector('.end-date' + input.id.replace('select', ''))?.innerText;
+
+      const data2 = { startdate, enddate };
+      localStorage.setItem("data2", JSON.stringify(data2));
+
+      if (input.checked) {
+        console.log(`start: ${startdate}, end: ${enddate}`);
+      }
+    });
+  });
+};
 
 function scrolltosection(sectionid) {
     
@@ -26,28 +48,20 @@ if (sectionid === 'page4'){
     let sectoin1, sectoin2, sectoin3, sectoin4, sectoin5, sectoin6;
     
     
-    if (departurestation === "khouribga") {
-      sectoin1 = "khouribga";
-    }
-    if (departurestation === "marrakech") {
-      sectoin2 = "marrakech";
-    }
-    if (departurestation === "tanger") {
-      sectoin3 = "tanger";
-    }
+    if (departurestation === "khouribga") { sectoin1 = "khouribga";}
+
+    if (departurestation === "marrakech") {sectoin2 = "marrakech";}
+
+    if (departurestation === "tanger") {sectoin3 = "tanger";}
     
-    if (destinationstation === "paris") {
-      sectoin4 = "paris";
-    }
-    if (destinationstation === "toulouse") {
-      sectoin5 = "toulouse";
-    }
-    if (destinationstation === "london") {
-      sectoin6 = "london";
-    }
+    if (destinationstation === "paris") {sectoin4 = "paris";}
+
+    if (destinationstation === "toulouse") {sectoin5 = "toulouse";}
+
+    if (destinationstation === "london") {sectoin6 = "london";}
      
     const data1 = { sectoin1, sectoin2, sectoin3, sectoin4, sectoin5, sectoin6, departuredate };
-    localStorage.setItem("data", JSON.stringify(data1));
+    localStorage.setItem("data1", JSON.stringify(data1));
     
     
     console.log(sectoin1, sectoin2, sectoin3, sectoin4, sectoin5, sectoin6 , departuredate );
@@ -58,33 +72,107 @@ if (sectionid === 'page4'){
         alert("please select");
         return;
         
+
         
     }
+// page 5
+if (sectionid === 'page5') {
+  console.log(sectionid);
+  let selectinput= false;
 
-    document.getElementById(sectionid).scrollIntoView({ behavior: 'smooth' }); }
+  const inputs = document.querySelectorAll(".inputs");
+  inputs.forEach(input => {
+    if(input.checked){
+      selectinput = true;
+    }
+  });
+  
+if (!selectinput) {
+  alert("please select an option")
+  return;
+  
+}
 
+}
 
 
 
+    document.getElementById(sectionid).scrollIntoView({ behavior: 'smooth' }); 
+  
 
+}
 
 
 
+const adultPrice = 500;
+const childPrice = 100;
+ 
 
 
+let adultcount = 0;
+let childcount = 0;
 
+function updatecount(type, change) {
+  checkbox();
+    if (type === 'adult') {
+        adultcount = Math.max(0, adultcount + change); 
+        document.getElementById('adult-count').innerText = adultcount;
+    } 
+    if (type === 'child') {
+        childcount = Math.max(0, childcount + change); 
+        document.getElementById('child-count').innerText = childcount;
+    }
 
+    totalprice();
+}
+let placeNum=0;
+function totalprice(){
 
+  const totalprice = (adultcount * adultPrice) + (childcount * childPrice) 
+placeNum=adultcount+childcount;
 
+  document.getElementById('total-price').innerText = totalprice +'dh';
 
 
+}
 
 
 
 
+let countChecked = 0;
+let inputCheck = document.querySelectorAll('input[type="checkbox"]');
+// let placeNum = 3; 
 
+inputCheck.forEach(input => {
+  input.setAttribute('disabled', true);
+  if (input.checked) {
+    countChecked++;
+  }
+});
 
+function checkbox() {
+  inputCheck.forEach(checkbox => {
+    checkbox.removeAttribute('disabled');
+    checkbox.addEventListener('change', () => {
+    
+      countChecked = 0;
+      inputCheck.forEach(input => {
+        if (input.checked) {
+          countChecked++;
+        }
+      });
 
+     
+      inputCheck.forEach(input => {
+        if (countChecked < placeNum) {
+          input.removeAttribute('disabled');
+        } else if (!input.checked) {
+          input.setAttribute('disabled', true);
+        }
+      });
+    });
+  });
+}
 
 
 
@@ -99,103 +187,57 @@ if (sectionid === 'page4'){
 
 
 
+// let countChecked=0
+// let inputCheck=document.querySelectorAll('input[type="checkbox"]');
+// for(let i=0;i<inputCheck.length;i++){
+//   inputCheck[i].setAttribute('disabled',true);
+//   if(inputCheck[i].checked){
+//     countChecked++;
+//   }
+// }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // function add() {
-// //     const input1 = document.getElementById("input1").value;
-// //     const input2 = document.getElementById("input2").value;
-// //     if (input1 .trim() === "" || input2.trim() === "") {
-// //         alert("please fill in both fields before proceeding.");}
-
-// //     const data = { input2, input1 };
-// //     localStorage.setItem("data", JSON.stringify(data));
-
-// //     console.log(input1, input2);
-// //     document.getElementById("input1").value= " "
-// //     document.getElementById("input2").value = " "
-
-
-
-// // }
-
-
-
-
-//     function add1() {
-        
-//         const departurestation = document.getElementById("departurestation").value;
-//         const destinationstation = document.getElementById("destinationstation").value;
-      
-//         let sectoin1, sectoin2, sectoin3, sectoin4, sectoin5, sectoin6;
-      
-       
-//         if (departurestation === "khouribga") {
-//           sectoin1 = "khouribga";
-//         }
-//         if (departurestation === "marrakech") {
-//           sectoin2 = "marrakech";
-//         }
-//         if (departurestation === "tanger") {
-//           sectoin3 = "tanger";
-//         }
-      
-//         if (destinationstation === "paris") {
-//           sectoin4 = "paris";
-//         }
-//         if (destinationstation === "toulouse") {
-//           sectoin5 = "toulouse";
-//         }
-//         if (destinationstation === "london") {
-//           sectoin6 = "london";
-//         }
-      
-       
-//         if (!sectoin1 && !sectoin2 && !sectoin3 && !sectoin4 && !sectoin5 && !sectoin6) {
-//           alert("please select");
-//           return; 
-//         }
-      
-        
-//         const departuredate = document.getElementById("departuredate").value;
-      
-       
-//         const data1 = { sectoin1, sectoin2, sectoin3, sectoin4, sectoin5, sectoin6, departuredate };
-//         localStorage.setItem("data", JSON.stringify(data1));
-      
-       
-//         console.log(sectoin1, sectoin2, sectoin3, sectoin4, sectoin5, sectoin6);
+// function checkbox(){
+  
+// inputCheck.forEach(checkbox=>{
+//   checkbox.removeAttribute("disabled");
+//   checkbox.addEventListener('change',()=>{
+//     for(let i=0;i<inputCheck.length;i++){
+//       if(inputCheck[i].checked){
+//         countChecked++;
 //       }
-      
+//     }
+//     inputCheck.forEach(checkbox=>{
+//       if(placeNum>countChecked){
+//         checkbox.removeAttribute('disabled');
+//       }else if(!checkbox.checked){
+//         checkbox.setAttribute('disabled',true);
+//       }
+//   })
+// })
+// })
+
+    
+  
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ jss.js
@@ -0,0 +1,91 @@
+
+
+    
+//       const inputs = document.querySelectorAll(".inputs"); 
+  
+//       inputs.forEach(input => {
+//         input.addEventListener('change', function () {
+//           const label = input.closest("label"); 
+          
+         
+//           const startDate = label.querySelector('.start-date' + input.id.replace('select', ''))?.innerText;
+//           const endDate = label.querySelector('.end-date' + input.id.replace('select', ''))?.innerText;
+          
+//           if (input.checked) {
+//             console.log(`Selected flight: Start: ${startDate}, End: ${endDate}`); 
+//           }
+//         });
+//       });
+    
+
+
+
+
+
+
+
+// // // function add() {
+// // //     const input1 = document.getElementById("input1").value;
+// // //     const input2 = document.getElementById("input2").value;
+// // //     if (input1 .trim() === "" || input2.trim() === "") {
+// // //         alert("please fill in both fields before proceeding.");}
+
+// // //     const data = { input2, input1 };
+// // //     localStorage.setItem("data", JSON.stringify(data));
+
+// // //     console.log(input1, input2);
+// // //     document.getElementById("input1").value= " "
+// // //     document.getElementById("input2").value = " "
+
+
+
+// // // }
+
+
+
+
+// //     function add1() {
+        
+// //         const departurestation = document.getElementById("departurestation").value;
+// //         const destinationstation = document.getElementById("destinationstation").value;
+      
+// //         let sectoin1, sectoin2, sectoin3, sectoin4, sectoin5, sectoin6;
+      
+       
+// //         if (departurestation === "khouribga") {
+// //           sectoin1 = "khouribga";
+// //         }
+// //         if (departurestation === "marrakech") {
+// //           sectoin2 = "marrakech";
+// //         }
+// //         if (departurestation === "tanger") {
+// //           sectoin3 = "tanger";
+// //         }
+      
+// //         if (destinationstation === "paris") {
+// //           sectoin4 = "paris";
+// //         }
+// //         if (destinationstation === "toulouse") {
+// //           sectoin5 = "toulouse";
+// //         }
+// //         if (destinationstation === "london") {
+// //           sectoin6 = "london";
+// //         }
+      
+       
+// //         if (!sectoin1 && !sectoin2 && !sectoin3 && !sectoin4 && !sectoin5 && !sectoin6) {
+// //           alert("please select");
+// //           return; 
+// //         }
+      
+        
+// //         const departuredate = document.getElementById("departuredate").value;
+      
+       
+// //         const data1 = { sectoin1, sectoin2, sectoin3, sectoin4, sectoin5, sectoin6, departuredate };
+// //         localStorage.setItem("data", JSON.stringify(data1));
+      
+       
+// //         console.log(sectoin1, sectoin2, sectoin3, sectoin4, sectoin5, sectoin6);
+// //       }
+      
